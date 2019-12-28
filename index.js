5513513e4395334a2d556d5cb1e2755950f6b445
tinaxd <31384595+tinaxd@users.noreply.github.com>
2019-12-28 17:49:17 +0900

    stop watch

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 let timerStatus = {
   mode: 'timer',
-  handler: null
-}
+  handler: null,
+  currSecond: 0,
+  running: false,
+  initTarget: 0
+};
 
 let timerModeBtn;
 let stopwatchModeBtn;
+let timerDisplay;
+
+function count() {
+  if (!timerStatus.running) return;
+  if (timerStatus.mode === 'stopwatch') {
+    timerStatus.currSecond++;
+  } else {
+    timerStatus.currSecond--;
+    checkTimerEnd();
+  }
+  updateView();
+}
+
+function checkTimerEnd() {
+  if (timerStatus.currSecond == 0) {
+    timerStatus.running = false;
+  }
+}
+
+function updateView() {
+  const minute = ('00' + Math.floor(timerStatus.currSecond / 60)).slice(-2);
+  const second = ('00' + timerStatus.currSecond % 60).slice(-2);
+  timerDisplay.innerHTML = minute + ':' + second;
+}
 
 function startTimer() {
-
+  if (timerStatus.running) return;
+  timerStatus.handler = setInterval(count, 1000);
+  timerStatus.running = true;
 }
 
 function stopTimer() {
-  
+  if (timerStatus.handler) {
+    clearInterval(timerStatus.handler);
+  }
+  timerStatus.running = false;
 }
 
 function resetTimer() {
-
+  timerStatus.currSecond = timerStatus.initTarget;
+  timerStatus.running = false;
+  updateView();
 }
 
 function changeTimerMode() {
@@ -42,4 +76,5 @@ function buttonUpdate(enabledMode) {
 window.onload = () => {
   timerModeBtn = document.getElementById('timerModeBtn');
   stopwatchModeBtn = document.getElementById('stopwatchModeBtn');
+  timerDisplay = document.getElementById('timerDisplay');
 }
